c21d3576e6c4904fb6cf76c459e0212f1a3a2260
Aaron Smith <32134610+aaroncolesmith@users.noreply.github.com>
2022-02-19 18:45:41 -0800

    update data file reference

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import plotly_express
 
-df=pd.read_csv('aaroncolesmith/club_soccer_rankings/club_soccer_rankings.csv')
+df=pd.read_csv('club_soccer_rankings.csv')
 
 st.title('FiveThirtyEight Global Club Soccer Rankings Over Time')
 st.markdown('[Data Credit: FiveThirtyEight Global Club Soccer Rankings](https://projects.fivethirtyeight.com/soccer-predictions/global-club-rankings/)')
