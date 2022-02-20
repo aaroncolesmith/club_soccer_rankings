0ad858e505acfc8cfa2d8682acb52f671a843b0f
Aaron Smith <32134610+aaroncolesmith@users.noreply.github.com>
2022-02-19 18:32:55 -0800

    add plotly

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 import pandas as pd
 import streamlit as st
+import plotly_express
+
+df=pd.read_csv('aaroncolesmith/club_soccer_rankings/club_soccer_rankings.csv')
 
 st.title('FiveThirtyEight Global Club Soccer Rankings Over Time')
 st.markdown('[Data Credit: FiveThirtyEight Global Club Soccer Rankings](https://projects.fivethirtyeight.com/soccer-predictions/global-club-rankings/)')
+
+px.scatter(df,
+    x='date',
+    y='rating',
+    color='team')
