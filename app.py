5b3306fd9590d0c6a8f33bd3507a85566a805c95
Aaron Smith <32134610+aaroncolesmith@users.noreply.github.com>
2022-02-19 18:58:21 -0800

    formatting

--- app.py
+++ app.py
@@ -6,10 +6,13 @@ df = pd.read_csv("club_soccer_rankings.csv")
 
 st.title("FiveThirtyEight Global Club Soccer Rankings Over Time")
 
-fig = px.line(df, x="date", y="rating", color="team", render_mode="svg", line_shape="spline")
+fig = px.line(
+    df, x="date", y="rating", color="team", render_mode="svg", line_shape="spline"
+)
 
 st.plotly_chart(fig, use_container_width=True)
 
 st.markdown(
     "[Data Credit: FiveThirtyEight Global Club Soccer Rankings](https://projects.fivethirtyeight.com/soccer-predictions/global-club-rankings/)"
 )
+
