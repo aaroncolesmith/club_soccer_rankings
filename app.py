b70b43829cac6da20d900f7be42045b15a222387
Aaron Smith <32134610+aaroncolesmith@users.noreply.github.com>
2022-02-19 18:22:59 -0800

    Creating app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import streamlit as st
+
+st.title('FiveThirtyEight Global Club Soccer Rankings Overtime')
+st.markdown('[Data Credit: FiveThirtyEight Global Club Soccer Rankings](https://projects.fivethirtyeight.com/soccer-predictions/global-club-rankings/)')
