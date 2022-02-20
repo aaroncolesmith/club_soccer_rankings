5f62470f80c5d908e21466ecab4b65d3f06bf076
Aaron Smith <32134610+aaroncolesmith@users.noreply.github.com>
2022-02-19 18:52:12 -0800

    fix formatting

--- app.py
+++ app.py
@@ -2,20 +2,21 @@ import pandas as pd
 import streamlit as st
 import plotly_express as px
 
-df=pd.read_csv('club_soccer_rankings.csv')
+df = pd.read_csv("club_soccer_rankings.csv")
 
-st.title('FiveThirtyEight Global Club Soccer Rankings Over Time')
-st.markdown('[Data Credit: FiveThirtyEight Global Club Soccer Rankings](https://projects.fivethirtyeight.com/soccer-predictions/global-club-rankings/)')
+st.title("FiveThirtyEight Global Club Soccer Rankings Over Time")
+st.markdown(
+    "[Data Credit: FiveThirtyEight Global Club Soccer Rankings](https://projects.fivethirtyeight.com/soccer-predictions/global-club-rankings/)"
+)
 
-fig=px.line(df,
-    x='date',
-    y='rating',
-    color='team',
-    render_mode='svg')
-fig.update_traces(mode='lines',
-    line_shape='spline',
-    opacity=.75
-    marker=dict(size=8,line=dict(width=1,color='DarkSlateGrey')),
-                            line = dict(width=4))
+fig = px.line(df, x="date", y="rating", color="team", render_mode="svg")
+fig.update_traces(
+    mode="lines",
+    line_shape="spline",
+    opacity=0.75,
+    marker=dict(size=8, line=dict(width=1, color="DarkSlateGrey")),
+    line=dict(width=4),
+)
 st.plotly_chart(fig, use_container_width=True)
+
 
