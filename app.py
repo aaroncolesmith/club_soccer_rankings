6cc31978f7d3d7d4d052a2aa2141e32f96cd0a15
Aaron Smith <32134610+aaroncolesmith@users.noreply.github.com>
2022-02-19 18:29:02 -0800

    overtime to Over Time

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pandas as pd
 import streamlit as st
 
-st.title('FiveThirtyEight Global Club Soccer Rankings Overtime')
+st.title('FiveThirtyEight Global Club Soccer Rankings Over Time')
 st.markdown('[Data Credit: FiveThirtyEight Global Club Soccer Rankings](https://projects.fivethirtyeight.com/soccer-predictions/global-club-rankings/)')
